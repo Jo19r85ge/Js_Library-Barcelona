4bcdb6dc745fb1c69aefd1cba865acee9d595f76
Jorge_JJG <48150256+Jo19r85ge@users.noreply.github.com>
2019-08-20 22:31:41 +0200

    Update app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -39,9 +39,11 @@ window.onclick = function(event)
     {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
+        
       for (i = 0; i < dropdowns.length; i++) 
       {
         var openDropdown = dropdowns[i];
+          
         if (openDropdown.classList.contains('show')) 
         {
             openDropdown.classList.remove('show');
